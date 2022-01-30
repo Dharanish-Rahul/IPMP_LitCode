4630ec52b6fa512babc98ba91b2bdbf8c2cd7a6f
Dharanish-Rahul <83532097+Dharanish-Rahul@users.noreply.github.com>
2022-01-30 22:59:02 +0530

    Add files via upload

---
+++ arrays/duplicates in o(n).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)cin>>arr[i];
+	
+	
+	
+	for (int i = 0; i < n; i++) {
+		arr[arr[i] % n]
+			= arr[arr[i] % n] + n;
+	}
+	cout << "The repeating elements are : " << endl;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] >= n * 2) {
+			cout << i << "\n";
+		}
+	}
+	return 0;
+}
+
+
+
