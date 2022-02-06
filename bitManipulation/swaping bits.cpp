1bf9015056c698a78d979c5199686c60e9f821d0
Dharanish-Rahul <83532097+Dharanish-Rahul@users.noreply.github.com>
2022-02-06 23:30:31 +0530

    Add files via upload

---
+++ bitManipulation/swaping bits.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to swap even
+// and odd bits
+unsigned int swapBits(unsigned int x)
+{
+    for(int i=0; i<32; i+=2){
+        int i_bit = ( x >> i ) & 1; 
+        int i_1_bit = (x >> ( i+1 )) & 1;  
+         
+        x = x - ( i_bit << i) 
+              - ( i_1_bit << ( i+1 ) ) 
+              + ( i_bit << ( i+1 ) )
+              + ( i_1_bit << i );  
+    }
+    return x;
+}
+ 
+
+int main()
+{
+    unsigned int x;
+	
+	cin>>x; 
+    
+ 
+    cout<<swapBits(x);
+ 
+    return 0;
+}
